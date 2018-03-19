87d9153a58973fb274e5d493a0322c326bddc7ce
Bruce Sun <brucesun@adobe.com>
2018-03-19 14:26:07 +0800

    Upgrade v2ray to version 3.14, add new formula dnscrypt-proxy version 2.0.7

---
+++ Formula/dnscrypt-proxy.rb
@@ -0,0 +1,20 @@
+class DnscryptProxy < Formula
+    desc "A flexible DNS proxy, with support for encrypted DNS protocols."
+    homepage "https://github.com/jedisct1/dnscrypt-proxy"
+    version "2.0.7"
+
+    url "https://github.com/jedisct1/dnscrypt-proxy/releases/download/#{version}/dnscrypt-proxy-macos-#{version}.tar.gz"
+    sha256 "16c85a33b9463c27ff946564b48398fa03c7fca5df4b7e2b43ac6c59513feff2"
+
+    bottle :unneeded
+
+    def install
+        libexec.install Dir["*"]
+        bin.install_symlink("#{libexec}/dnscrypt-proxy")
+    end
+
+    def caveats; <<~EOS
+        Executable linked as "dnscrypt-proxy".
+        EOS
+    end
+end
--- Formula/v2ray.rb
+++ Formula/v2ray.rb
@@ -1,7 +1,7 @@
 class V2ray < Formula
     desc "A platform for building proxies to bypass network restrictions."
     homepage "https://www.v2ray.com"
-    version "3.13"
+    version "3.14"
 
     option "with-mirror", "Download installation archive from https://www.v2ray.com/download, use this option if you're in mainland China for a faster downloading speed."
 
@@ -11,7 +11,7 @@ class V2ray < Formula
         url "https://github.com/v2ray/v2ray-core/releases/download/v#{version}/v2ray-macos.zip"
     end
 
-    sha256 "9dd60c7cef08f542d811fab3e7bb21ab3bf2cdd8e03bb375a6d7cbdea8653f51"
+    sha256 "8ac788cab01289ebf964c724d35eeb9c458a370e204956cf1a2ca3f5c433e961"
 
     bottle :unneeded
 
@@ -19,5 +19,10 @@ class V2ray < Formula
         libexec.install Dir["*"]
         bin.install_symlink("#{libexec}/v2ray")
         bin.install_symlink("#{libexec}/v2ctl")
+    end
+
+    def caveats; <<~EOS
+        Executable linked as "v2ray" and "v2ctl".
+        EOS
     end
 end
