bf6ac46f514fee68e18cf5766eb75a683b744251
okampfer <okampfer@gmail.com>
2018-10-15 09:06:50 +0800

    Upgrade v2ray-core to version 3.47

--- Formula/v2ray-core.rb
+++ Formula/v2ray-core.rb
@@ -1,10 +1,17 @@
 class V2rayCore < Formula
     desc "A platform for building proxies to bypass network restrictions."
     homepage "https://github.com/v2ray/v2ray-core"
-    version "3.37"
+    version "3.47"
 
-    url "https://github.com/v2ray/v2ray-core/releases/download/v#{version}/v2ray-macos.zip"
-    sha256 "aebf36b20e9b3e1a5897de7367b9e6974ffbe3198cd2d93a0bf27dd96bb0755c"
+    option "with-mirror", "Download from https://v2ray.com/download."
+
+    if build.with? "mirror"
+        url "https://v2ray.com/download/Core_v#{version}/v2ray-macos.zip"
+    else
+        url "https://github.com/v2ray/v2ray-core/releases/download/v#{version}/v2ray-macos.zip"
+    end
+
+    sha256 "bd594f1068d7b5d9643f4c1ddec7bd129540bfc64ed9fefc90d0feec018f20c8"
 
     bottle :unneeded
 
