b73949290429788cac8441100f6c27ecc789fb67
okampfer <okampfer@gmail.com>
2019-01-09 11:43:53 +0800

    Upgrade baidu-pcs-go to version 3.5.6

--- Formula/baidu-pcs-go.rb
+++ Formula/baidu-pcs-go.rb
@@ -1,10 +1,10 @@
 class BaiduPcsGo < Formula
     desc "Baidu Personal-Cloud-Storage CLI client."
     homepage "https://github.com/iikira/BaiduPCS-Go"
-    version "3.5.4"
+    version "3.5.6"
 
     url "https://github.com/iikira/BaiduPCS-Go/releases/download/v#{version}/BaiduPCS-Go-v#{version}-darwin-osx-amd64.zip"
-    sha256 "d13dcca9c7b7705340ab8820a2a5c3350523f9dd39c64d1cfcdf96932ecf5987"
+    sha256 "db087e64d606639ca3e05a4b482174a06d3c0eabd6cbdbcc317374fc40742a96"
 
     bottle :unneeded
 
