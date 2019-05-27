80d508d2053b74499ecbad3226f1d1fdc6858c5e
okampfer <okampfer@gmail.com>
2019-05-27 09:55:30 +0800

    Upgrade go-proxy to version 7.4

--- Formula/go-proxy.rb
+++ Formula/go-proxy.rb
@@ -1,10 +1,10 @@
 class GoProxy < Formula
     desc "A high performance HTTP(S), websocket, TCP, UDP,Secure DNS, Socks5 proxy server implemented by golang. Supports chain-style proxies,nat forwarding in different lan,TCP/UDP port forwarding, SSH forwarding."
     homepage "https://github.com/snail007/goproxy"
-    version "6.5"
+    version "7.4"
 
     url "https://github.com/snail007/goproxy/releases/download/v#{version}/proxy-darwin-amd64.tar.gz"
-    sha256 "134ece99bfb5b9be0e387e2884519df39debe4cb2421d96a8c965614dbca0ba7"
+    sha256 "c70800f33b4bebe3b267b32a967331733b80928ca7f1ec5e3d5bee2bd668a279"
 
     bottle :unneeded
 
