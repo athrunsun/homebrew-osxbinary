cca7be34cde2ace7258354174a94aace3d07a686
okampfer <okampfer@gmail.com>
2019-08-20 22:31:06 +0800

    Upgrade docker-ce-client-binary to version 19.03.1

--- Formula/docker-ce-client-binary.rb
+++ Formula/docker-ce-client-binary.rb
@@ -1,10 +1,10 @@
 class DockerCeClientBinary < Formula
     desc "Pack, ship and run any application as a lightweight container. This is the community edition, and includes only client binary (no dockerd daemon)."
-    homepage "https://docs.docker.com/install/linux/docker-ce/binaries"
-    version "18.09.0"
+    homepage "https://docs.docker.com/install/linux/docker-ce/binaries/#install-client-binaries-on-macos"
+    version "19.03.1"
 
     url "https://download.docker.com/mac/static/stable/x86_64/docker-#{version}.tgz"
-    sha256 "1c68be323e36d0ddb03867bc3a0ef89e9e92532d42b63abb9a1ef19e06d2015c"
+    sha256 "8c30cee81e90f6d462823201e62243ceb4be954ec6a4c6e5391ebece3b10e73a"
 
     bottle :unneeded
     conflicts_with "docker", :because => "both install `docker` binaries"
