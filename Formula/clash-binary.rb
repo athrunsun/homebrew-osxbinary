5701fad84dc7a43ed22a86ee42cac5dba6f01aab
okampfer <okampfer@foxmail.com>
2022-01-25 01:25:31 +0800

    Fix incorrect ruby syntax in clash-binary.

--- Formula/clash-binary.rb
+++ Formula/clash-binary.rb
@@ -3,13 +3,13 @@ class ClashBinary < Formula
     homepage "https://github.com/Dreamacro/clash"
     version "1.9.0"
 
-    arch = "amd64"
+    @arch = "amd64"
 
     if Hardware::CPU.arm?
-        arch = "arm64"
+        @arch = "arm64"
     end
 
-    url "https://github.com/Dreamacro/clash/releases/download/v#{version}/clash-darwin-#{arch}-v#{version}.gz"
+    url "https://github.com/Dreamacro/clash/releases/download/v#{version}/clash-darwin-#{@arch}-v#{version}.gz"
 
     if Hardware::CPU.arm?
         sha256 "c6f1622b0fe779a30d3fdcd12c1e88cab24edd74dd5c4c851ab6ad3a87304533"
@@ -19,7 +19,7 @@ class ClashBinary < Formula
 
     def install
         libexec.install Dir["*"]
-        bin.install_symlink("#{libexec}/clash-darwin-#{arch}" => "clash")
+        bin.install_symlink("#{libexec}/clash-darwin-#{@arch}" => "clash")
     end
 
     def caveats; <<~EOS
