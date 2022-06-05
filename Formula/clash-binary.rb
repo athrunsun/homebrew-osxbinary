4f164711805bb07aba917bd61782da15dbc36521
Bruce Sun <bruce@workstream.is>
2022-06-05 15:36:33 +0800

    Upgrade clash-binary to version 1.10.6

--- Formula/clash-binary.rb
+++ Formula/clash-binary.rb
@@ -1,26 +1,27 @@
 class ClashBinary < Formula
     desc "A rule-based tunnel in Go."
     homepage "https://github.com/Dreamacro/clash"
-    version "1.9.0"
+    version "1.10.6"
 
+    @@os = "darwin"
     @@arch = "amd64"
 
     if Hardware::CPU.arm?
         @@arch = "arm64"
     end
 
-    url "https://github.com/Dreamacro/clash/releases/download/v#{version}/clash-darwin-#{@@arch}-v#{version}.gz"
+    url "https://github.com/Dreamacro/clash/releases/download/v#{version}/clash-#{@@os}-#{@@arch}-v#{version}.gz"
+    sha256 "4716d1b2e22825fbede547841a1076473c18ae5a0f4f46417f3d9054dd868598"
 
     if Hardware::CPU.arm?
-        sha256 "c6f1622b0fe779a30d3fdcd12c1e88cab24edd74dd5c4c851ab6ad3a87304533"
-    else
-        sha256 "6195fcfc9018802f409d2234f17bb51c3e31a446b0ce570f2f0abb7ca354d443"
+        sha256 "25f1c9effb734639d1e2176a4fd2e69ce10eb4b727f6af27cb0801b4a8eec538"
     end
 
     def install
         libexec.install Dir["*"]
-        chmod(0755, "#{libexec}/clash-darwin-#{@@arch}")
-        bin.install_symlink("#{libexec}/clash-darwin-#{@@arch}" => "clash")
+        mv("#{libexec}/clash-#{@@os}-#{@@arch}", "#{libexec}/clash")
+        chmod(0755, "#{libexec}/clash")
+        bin.install_symlink("#{libexec}/clash" => "clash")
     end
 
     def caveats; <<~EOS
